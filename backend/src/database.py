47518f6b8b667a4eae27b80b186c66c304808c5a
Ethan Triem <EJTriem@outlook.com>
2025-03-23 00:39:25 -0500

    hid pass and user

--- backend/src/database.py
+++ backend/src/database.py
@@ -7,7 +7,7 @@ load_dotenv("backend.env")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/website_data"
+#DATABASE_URL = "postgresql://postgres:postgres@db:5432/website_data"
 database = Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
 engine = sqlalchemy.create_engine(DATABASE_URL)
