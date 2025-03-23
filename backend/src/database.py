59e7c62ffd952179cc659871a2a840d6dd5c1e08
Ethan Triem <EJTriem@outlook.com>
2025-03-23 00:34:34 -0500

    hidden db pass and user

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,5 +1,11 @@
 from databases import Database
 import sqlalchemy
+import os
+from dotenv import load_dotenv
+
+load_dotenv("backend.env")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASE_URL = "postgresql://postgres:postgres@db:5432/website_data"
 database = Database(DATABASE_URL)
